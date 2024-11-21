36e8216cb0cab773be2a6918e3bfab388d8cef63
Nitin Budakoti <manuboda8885@gmail.com>
2024-11-21 12:39:25 +0530

    Added new components and updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import NavBar from './components/NAVBAR';
 import AboutUs from './components/About';
 import Services from './components/Services';
 import Header from './components/Header';
-
-
-
-
+import Portfolio from './components/Portfolio';
+import Projects from './components/Projects';
+import Differentiators from './components/Differentiators';
+import Technology from './components/Technology';
+import Support from './components/Support';
+import Reference from './components/refernces';
+import Footer from './components/footer';
 export default function HomePage() {
     return (
         <div>
@@ -14,7 +17,15 @@ export default function HomePage() {
             <Header/>
             <AboutUs/>
             <Services/>
-         
+            <Portfolio/>
+            <Projects/>
+            <Differentiators/>
+            <Technology/>
+            <Support/>
+            <Reference/>
+            <Footer/>
+    
+
         </div>
     );
 }
